b3246c5783ec82cfdee0b6ebf9720aa80648c233
Oliwer Smedjevik <oliwer.smedjevik@gmail.com>
2019-03-20 12:58:05 +0100

    Adding configs for complete bundle

--- src/lib/config.js
+++ src/lib/config.js
@@ -10,12 +10,75 @@ const defaultConfig = {
 	storeConsentGlobally: false,
 	storePublisherData: false,
 	logging: false,
-	localization: {},
+    localization: {
+        sv: {
+            banner: {
+                title:
+                    'Annonser hjälper oss att driva denna site',
+                description:
+                    'När du besöker vår webbplats kan förvalda företag komma åt och använda viss information på din enhet och om den här webbplatsen för att visa relevanta annonser eller personligt innehåll.',
+                links: {
+                    data: {
+                        title:
+                            'Information som kan användas',
+                        description: ''
+                    },
+                    purposes: {
+                        title:
+                            'Syfte för lagring av information',
+                        description:
+                            'Hur informationen kan anvädas:'
+                    },
+                    manage: 'Ändra inställningar',
+                    accept: 'Fortsätt till site'
+                }
+            },
+            summary: {
+                title: 'Learn more about how information is being used?',
+                description: "We and select companies may access and use your information for the below purposes. You may customize your choices below or continue using our site if you're OK with the purposes.",
+                who: {
+                    title: 'Who is using this information?',
+                    description: `We and pre-selected companies will use your information. You can see each company in the links above or see the complete list here.`
+                },
+                what: {
+                    title: '',
+                    description: ''
+                }
+            },
+            details: {
+                back: '',
+                save: ''
+            },
+            purposes: {
+                title: '',
+                description: '',
+                back: '',
+                optoutdDescription: ``,
+            },
+            vendors: {
+                title: '',
+                description: '',
+                accept: '',
+                acceptAll: '',
+                acceptNone: '',
+                optOut: '',
+                back: ''
+            }
+        }
+    },
 	forceLocale: null,
 	gdprApplies: true,
 	gdprAppliesGlobally: false,
-	allowedVendorIds: null,
-	theme: {}
+	allowedVendorIds: [1, 2, 3],
+    theme: {
+        primaryColor: '#2D9CDB',
+        primaryTextColor: '#fff',
+        secondaryColor: '#626262',
+        secondaryTextColor: '#fff',
+        textLinkColor: '#2D9CDB',
+        textColor: 'rgba(0, 0, 0, 0.8)',
+        textLightColor: 'rgba(0, 0, 0, 0.5)'
+    }
 };
 
 class Config {
