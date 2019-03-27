2ccd925d630fe6c1e4a307a23509d96d7efc9534
Oliwer Smedjevik <oliwer.smedjevik@gmail.com>
2019-03-27 11:48:16 +0100

    Changing cofig

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,10 +3,9 @@ import log from './log';
 // These options are documented at https://acdn.adnxs.com/cmp/docs/#/config
 // We highly recommend reading the options as the defaults may not fit your goals.
 const defaultConfig = {
-	customPurposeListLocation: './purposes.json',
-	// The location of the latest vendorlist to use.
-	globalVendorListLocation: 'https://vendorlist.consensu.org/vendorlist.json',
-	globalConsentLocation: './portal.html',
+	customPurposeListLocation: '/purposes.json',
+	globalVendorListLocation: '/vendorlist.json',
+	globalConsentLocation: '/portal.html',
 	storeConsentGlobally: false,
 	storePublisherData: false,
 	logging: false,
@@ -18,48 +17,77 @@ const defaultConfig = {
 					'När du besöker vår webbplats kan förvalda företag komma åt och använda viss information på din enhet och om den här webbplatsen för att visa relevanta annonser eller personligt innehåll.',
 				links: {
 					data: {
-						title: 'Information som kan användas',
-						description: ''
+						title: 'Information som kan komma att användas.',
+						description: `<ul>
+                                        <li>Typ av webbläsare och inställningar</li>
+                                        <li>Information om enhetens operativsystem</li>
+                                        <li>Information om cookies</li>
+                                        <li>Information om andra identifierare som tilldelats enheten</li>
+                                        <li>Den IP-adress från vilken enheten får åtkomst till en kunds webbplats eller mobilapplikation</li>
+                                        <li>Information om användarens aktivitet på den enheten, inklusive webbsidor och mobilappar som har besökts eller använts</li>
+                                        <li>Information om den geografiska platsen för enheten när den kommer åt en webbplats eller mobilapplikation</li>
+                                    </ul>
+                        `
 					},
 					purposes: {
-						title: 'Syfte för lagring av information',
-						description: 'Hur informationen kan anvädas:'
+						title: 'Syfte för lagring av information.',
+						description: `<ul>
+                                        <li>Informationslagring och åtkomst</li>
+                                        <li>Anpassning</li>
+                                        <li>Annonsval, leverans, rapportering</li>
+                                        <li>Innehållsval, leverans, rapportering</li>
+                                        <li>Mått</li>
+                                    </ul>
+                        `
 					},
 					manage: 'Ändra inställningar',
-					accept: 'Fortsätt till site'
+					accept: 'Fortsätt till webbplats'
 				}
 			},
 			summary: {
-				title: 'Learn more about how information is being used?',
+				title: 'Läs mer om hur information används?',
 				description:
-					"We and select companies may access and use your information for the below purposes. You may customize your choices below or continue using our site if you're OK with the purposes.",
+					'Vi och valda företag kan komma åt och använda din information för nedanstående ändamål. Du kan anpassa dina val nedan eller fortsätta använda vår webbplats om du är okej med syftet.',
+				detailLink: 'Läs Mer & Ange Inställningar',
 				who: {
-					title: 'Who is using this information?',
-					description: `We and pre-selected companies will use your information. You can see each company in the links above or see the complete list here.`
+					title: 'Vem använder den här informationen?',
+					description: `Vi och förvalda företag kommer att använda din information. Du kan se varje företag i länkarna ovan eller`,
+					link: 'se hela listan här.'
 				},
 				what: {
-					title: 'What title',
-					description: ''
+					title: 'Vilken information används?',
+					description: 'Olika företag använder olika uppgifter,',
+					link: 'se den fullständiga listan här.'
 				}
 			},
 			details: {
-				back: '',
-				save: ''
+				back: 'Tillbaka',
+				save: 'Forstätt till webbplats'
 			},
 			purposes: {
-				title: '',
-				description: '',
-				back: '',
-				optoutdDescription: ``
+				title: 'Vilken information används?',
+				description:
+					'Nedan finns en komplett lista över de uppgifter som kan samlas in.',
+				back: 'Tillbaka',
+				optoutdDescription: `Beroende på vilken typ av data de samlar in, använder, behandlar och andra faktorer, inklusive integritet genom design, är vissa partners beroende av ditt samtycke medan andra kräver att du avregistrerar dig. För information om varje leverantör och möjligheten att utöva dina val, se nedan. Vill du välja bort, besök då <a href="http://optout.networkadvertising.org/?c=1#!/" target="_blank">NAI</a>, <a href="http://optout.aboutads.info/?lang=EN&c=2#!/" target="_blank">DAA</a> eller <a href="http://www.youronlinechoices.com/" target="_blank">EDAA</a> för mer information.`,
+				items: `<ul>
+                            <li>Typ av webbläsare och dess inställningar</li>
+                            <li>Information om enhetens operativsystem</li>
+                            <li>Information om cookies</li>
+                            <li>Information om andra identifierare som tilldelats enheten</li>
+                            <li>Den IP-adress från vilken enheten får åtkomst till en kunds webbplats eller mobilapplikation</li>
+                            <li>Information om användarens aktivitet på den enheten, inklusive webbsidor och mobilappar som har besökts eller använts</li>
+                            <li>Information om den geografiska platsen för enheten när den kommer åt en webbplats eller mobilapplikation</li>
+                        </ul>`
 			},
 			vendors: {
 				title: '',
 				description: '',
-				accept: '',
-				acceptAll: '',
-				acceptNone: '',
+				accept: 'Acceptera',
+				acceptAll: 'Tillåt alla',
+				acceptNone: 'Tillåt ingen',
 				optOut: '',
-				back: ''
+				back: 'Tillbaka'
 			}
 		}
 	},
